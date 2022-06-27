a3b70723afb49afd1284c24306cdd86193f8114b
GitHub Action <action@github.com>
2022-06-27 11:05:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/baev/ideanavigatejsonpath/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/baev/ideanavigatejsonpath/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.baev.ideanavigatejsonpath.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.baev.ideanavigatejsonpath.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/baev/ideanavigatejsonpath/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/baev/ideanavigatejsonpath/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.baev.ideanavigatejsonpath.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.baev.ideanavigatejsonpath.MyBundle
 
 class MyApplicationService {
 
